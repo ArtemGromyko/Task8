cc373102926ea31c5ff84e8151ef1953608f1bc9
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-10-19 16:25:58 +0300

    Добавьте файлы проекта.

---
+++ Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = new string[] { "asd", "фвы", null, "!!!a" };
+            text = TextTransformer.UpperCase(text);
+            foreach (string str in text)
+                Console.WriteLine(str);
+            Console.WriteLine("______");
+
+            text = TextTransformer.ConvertNullString(text);
+            foreach (string str in text)
+                Console.WriteLine(str);
+        }
+    }
+}
---
+++ Task8/TextTransformer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task8
+{
+    static class TextTransformer
+    {
+        public static string[] UpperCase(string[] text)
+        {
+            string[] newText = new string[text.Length];
+            int index = 0;
+            foreach (string str in text)
+            {
+                if (str != null)
+                    newText[index++] = str.ToUpper();
+                else
+                    newText[index++] = str;
+            }
+            return newText;
+        }
+        public static string[] ConvertNullString(string[] text)
+        {
+            string[] newText = new string[text.Length];
+            int index = 0;
+            foreach (string str in text)
+            {
+                if (str == null)
+                    newText[index++] = "this is null string";
+                else
+                    newText[index++] = str;
+            }
+            return newText;
+        }
+    }
+}
